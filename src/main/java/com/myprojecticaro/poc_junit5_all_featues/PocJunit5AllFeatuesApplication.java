6b504847c20f4249d8ec97e73531f7eb858fdf57
IcaroCaetano <icaro.caetano@gmail.com>
2026-03-29 21:27:57 -0300

    first commit

---
+++ src/main/java/com/myprojecticaro/poc_junit5_all_featues/PocJunit5AllFeatuesApplication.java
@@ -0,0 +1,13 @@
+package com.myprojecticaro.poc_junit5_all_featues;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocJunit5AllFeatuesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PocJunit5AllFeatuesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/myprojecticaro/poc_junit5_all_featues/PocJunit5AllFeatuesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myprojecticaro.poc_junit5_all_featues;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PocJunit5AllFeatuesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
