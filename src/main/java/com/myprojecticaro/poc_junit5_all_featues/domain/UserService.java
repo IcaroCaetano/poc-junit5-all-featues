aa14a75c297bfddc242f2775a8c2043a4c0f1440
IcaroCaetano <icaro.caetano@gmail.com>
2026-03-31 23:21:28 -0300

    feat: add parametered tests

---
+++ src/main/java/com/myprojecticaro/poc_junit5_all_featues/domain/UserService.java
@@ -0,0 +1,34 @@
+package com.myprojecticaro.poc_junit5_all_featues.domain;
+
+public class UserService {
+
+    public boolean isValidUsername(String username) {
+        if (username == null || username.isBlank()) {
+            return false;
+        }
+        return username.length() >= 3 && username.length() <= 10;
+    }
+
+    public int calculateDiscount(int age) {
+        if (age < 0) {
+            throw new IllegalArgumentException("Invalid age");
+        }
+
+        if (age < 18) {
+            return 10;
+        } else if (age >= 60) {
+            return 20;
+        }
+        return 0;
+    }
+
+    public String getUserCategory(int age) {
+        if (age < 0) {
+            throw new IllegalArgumentException("Invalid age");
+        }
+
+        if (age < 18) return "MINOR";
+        if (age < 60) return "ADULT";
+        return "SENIOR";
+    }
+}
