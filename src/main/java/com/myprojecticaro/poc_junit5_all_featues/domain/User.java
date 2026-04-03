b8ef1b61e50820f12f13c0d3968be00c44b4d3c5
IcaroCaetano <icaro.caetano@gmail.com>
2026-04-02 23:14:09 -0300

    feat: add advanced assertions

---
+++ src/main/java/com/myprojecticaro/poc_junit5_all_featues/domain/User.java
@@ -0,0 +1,28 @@
+package com.myprojecticaro.poc_junit5_all_featues.domain;
+
+import java.util.List;
+
+public class User {
+
+    private String name;
+    private int age;
+    private List<String> roles;
+
+    public User(String name, int age, List<String> roles) {
+        this.name = name;
+        this.age = age;
+        this.roles = roles;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public List<String> getRoles() {
+        return roles;
+    }
+}
