d07c37596f11ce84f3af7e98e6fe132a9e926d5d
IcaroCaetano <icaro.caetano@gmail.com>
2026-04-04 18:40:27 -0300

    feat: add mockito

---
+++ src/test/java/com/myprojecticaro/poc_junit5_all_featues/mockito/domain/UserServiceTest.java
@@ -0,0 +1,58 @@
+package com.myprojecticaro.poc_junit5_all_featues.mockito.domain;
+
+import com.myprojecticaro.poc_junit5_all_featues.mockito.model.User;
+import com.myprojecticaro.poc_junit5_all_featues.mockito.repository.UserRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.*;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(org.mockito.junit.jupiter.MockitoExtension.class)
+class UserServiceTest {
+
+    @Mock
+    private UserRepository repository;
+
+    @InjectMocks
+    private UserService userService;
+
+    @Test
+    void shouldRegisterUserSuccessfully() {
+
+        String email = "test@email.com";
+
+        when(repository.findByEmail(email))
+            .thenReturn(Optional.empty());
+
+        when(repository.save(any(User.class)))
+            .thenAnswer(invocation -> invocation.getArgument(0));
+
+
+        User result = userService.registerUser(email);
+
+        assertEquals(email, result.getEmail());
+
+        verify(repository).findByEmail(email);
+        verify(repository).save(any(User.class));
+    }
+
+    @Test
+    void shouldThrowExceptionWhenUserAlreadyExists() {
+
+        String email = "test@email.com";
+
+        when(repository.findByEmail(email))
+            .thenReturn(Optional.of(new User(email)));
+
+        assertThrows(IllegalArgumentException.class, () -> {
+            userService.registerUser(email);
+        });
+
+        verify(repository).findByEmail(email);
+        verify(repository, never()).save(any());
+    }
+}
